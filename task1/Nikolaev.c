e5514d4d6e98e03ea9faccb5479a065f01a5f104
Николаев Денис Андреевич <s23380330@unn.ru>
2023-09-22 15:15:05 +0300

    I created chess

---
+++ task1/Nikolaev.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+  setlocale(LC_ALL,"");
+  int x1, y1, x2, y2;
+
+  printf("Введите координаты x1, y1, x2, y2 (0...8): ");
+  scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+
+  if (x1 == x2 && x2==y1 && y1==y2){
+    printf("Нет таких фигур");
+    return 0;
+  }
+
+  if ((abs(x2-x1)==1 && abs(y2-y1)==2) || (abs(x2-x1)==2 && abs(y2-y1)==1)){
+    printf("Конь\n");
+  }
+
+  if (x1==x2 || y1==y2){
+    printf("Слон\n");
+  }
+
+  if ((abs(x2-x1)==1 && abs(y2-y1)==1) ||
+       (abs(x2-x1)==1 && abs(y2-y1)==0) ||
+       (abs(x2-x1)==0 && abs(y2-y1)==1)){
+    printf("Король\n");
+  }
+
+  if (abs(x2-x1)==abs(y2-y1)){
+    printf("Ладья\n");
+  }
+
+  if ((abs(x2-x1)==abs(y2-y1)) || (x1==x2 || y1==y2)){
+    printf("Ферзь\n");
+  }
+
+  return 0;
+}
