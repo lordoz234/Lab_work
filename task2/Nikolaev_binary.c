10e87d1454fdb605418732419492d2fbc6fe49c4
Николаев Денис Андреевич <s23380330@unn.ru>
2023-09-29 15:38:36 +0300

    Added counter of tries for task 1

--- task2/Nikolaev_binary.c
+++ task2/Nikolaev_binary.c
@@ -11,31 +11,53 @@ void main(){
         int num = rand() % 1001;
         int guess;
         int k = 0;
+        int counter = 0;
 
-        printf("%d", num);
+        //printf("%d", num);
 
         printf("Guess a number(0..1000): ");
         scanf("%d", &guess);
 
         while (k==0){
             if (guess == num){
-                printf("You did it! Its %d", num);
+                printf("You did it! Its %d \n", num);
                 k++;
+                counter++;
             }
             else if (guess > num){
                 printf("Guess lower!");
+                counter++;
                 scanf("%d", &guess);
             }
             else if (guess < num){
                 printf("Guess higher!");
+                counter++;
                 scanf("%d", &guess);
             }
         }
+        printf("You number of guesses is %d", counter);
 
     }
 
     if (mode == 2){ // mode 2
         srand(0);
+        int num;
+        int left = 0;
+        int right = 1000;
+        char ch;
+        printf("Pick a number(0..1000): ");
+        scanf("%d", &num);
 
+        printf("%d \n", rand()%1001);
+        scanf("%c", &ch);
+        if (ch == "="){
+            printf("nice!");
+        }
+        else if(ch == ">"){
+            printf("so its bigger");
+        }
+        else{
+            printf("so its smaller");
+        }
     }
 }
