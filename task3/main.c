a3ceedeb9b5c4383b97c7aefe7d64fb38cb82a9d
Devlikamov Vlad <vlad.devlikamov@gmail.com>
2023-10-01 02:44:40 +0300

    Added qsort

--- task3/main.c
+++ task3/main.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+    const int* num1 = (const int*)a;
+    const int* num2 = (const int*)b;
+
+    return (*num1 - *num2);
+}
 
 int main() {
     int n;
@@ -7,6 +15,7 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%d", &array[i]);
     }
+    qsort(array, n, sizeof(int), compare);
     for (int i = 0; i < n; i++) {
         printf("%d ", array[i]);
     }
