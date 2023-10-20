e2780347b21f9c37bfbd0f51d19effff19b4f6e2
Ksunyy <126989113+Ksunyy@users.noreply.github.com>
2023-10-21 02:41:45 +0300

    Shvetsova.c

--- task3/Shvetsova.c
+++ task3/Shvetsova.c
@@ -68,7 +68,7 @@
 //     for (int i = 0; i < n; i++) {
 //         scanf("%d", &array[i]);
 //     }
-//     recurs(array, 0, n);
+//     recurs(array, 0, n-1);
 //     for (int i = 0; i < n; i++) {
 //         printf("%d ", array[i]);
 //     }
