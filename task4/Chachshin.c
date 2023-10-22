5d8007149f34a48ccedf9bf866f9ac01814cde22
kodt2 <kodt2005@yandex.ru>
2023-10-22 21:02:03 +0300

    changes

--- task4/Chachshin.c
+++ task4/Chachshin.c
@@ -18,11 +18,11 @@ int rec(int (x), int(y), int(wh), int(lh), int( **a)) {
 
 }
 
-#define release
+#define debug
 
 void main() {
 	srand(3);
-	int wh = 0, lh = 0, res = 0;
+	int wh = 0, lh = 0, res = 0, ** a;
 	printf("enter width and length of maze: ");
 	scanf_s("%d %d", &wh, &lh);
 
@@ -30,31 +30,31 @@ void main() {
 	printf("%d %d \n", wh, lh);
 	#endif
 
-	int** a = malloc(lh * sizeof(int));
-	for (int i = 0; i < lh; i++) {
-		a[i] = malloc(wh * sizeof(int));
+	a = (int**) malloc(wh * sizeof(int));
+	for (int i = 0; i < wh; i++) {
+		a[i] = (int*)malloc(lh * sizeof(int));
 	}
 
-	for (int i = 0; i < lh; i++) {
-		for (int j = 0; j < wh; j++) {
+	for (int i = 0; i < wh; i++) {
+		for (int j = 0; j < lh; j++) {
 
-			#ifdef release
+			#ifdef debug
 			a[i][j] = rand()%2;
 			#endif
-			#ifdef debug
+			#ifdef release
 			scanf_s(" %d", &a[i][j]);
 			#endif
 		}	
 	}
 	a[0][0] = 1;
-	a[lh - 1][wh - 1] = 1;
-	for (int i = 0; i < lh; i++) {
-		for (int j = 0; j < wh; j++) {
+	a[wh - 1][lh - 1] = 1;
+	for (int i = 0; i < wh; i++) {
+		for (int j = 0; j < lh; j++) {
 			printf("%d ",a[i][j]);
 		}
 		printf("\n");
 	}
-	res = rec(0, 0, lh, wh, a);
+	res = rec(0, 0, wh, lh, a);
 
 	#ifdef debug
 	printf("%d", res);
