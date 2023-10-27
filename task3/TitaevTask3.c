d44e7b934197404db82361d06c083f1b297380b1
Титаев Максим Алексеевич <s23380315@unn.ru>
2023-10-27 15:00:58 +0300

    Fix scanf

--- task3/TitaevTask3.c
+++ task3/TitaevTask3.c
@@ -4,7 +4,7 @@
 void sort(int arr[], int n) {
     for (int i = 0; i < n; i++) {
         int kol = 0;
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < n - 1; j++) {
             if (arr[j + 1] < arr[j]) {
                 int a = arr[j];
                 arr[j] = arr[j + 1];
